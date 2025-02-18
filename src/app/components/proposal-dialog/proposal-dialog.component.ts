55ca6d60e30bdc6b5909a3fce05a8db285f5d671
Gabriela411 <gabriela.toncu@yahoo.com>
2025-02-18 16:19:14 +0000

    Remove unnecessary loads & use users and items from store

--- src/app/components/proposal-dialog/proposal-dialog.component.ts
+++ src/app/components/proposal-dialog/proposal-dialog.component.ts
@@ -5,7 +5,7 @@ import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/fo
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import { selectCurrentUserId } from '../../../store/user/user.selectors';
+import { selectCurrentUserId, selectUsers } from '../../../store/user/user.selectors';
 import { Observable, take } from 'rxjs';
 import { DataService } from '../../services/data.services';
 import { Store } from '@ngrx/store';
@@ -20,67 +20,64 @@ import { User } from '../../models/user.model';
   styleUrls: ['./proposal-dialog.component.css'],
   imports: [MatFormField, MatLabel, MatButton, CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatInput],
 })
-export class ProposalDialogComponent implements OnInit {
-  comment: string = '';
-  paymentRatios: { [ownerId: number]: number } = {};
-  currentUserId$: Observable<number | null>;
-  usersFromFile: User[] = [];
+export class ProposalDialogComponent {
+    comment: string = '';
+    paymentRatios: { [ownerId: number]: number } = {};
+    currentUserId$: Observable<number | null>;
+    users$: Observable<User[]>; 
 
-  constructor(
-    public dialogRef: MatDialogRef<ProposalDialogComponent>,
-    private dataService: DataService,
-    private store: Store,
-    @Inject(MAT_DIALOG_DATA) 
-    public data: { selectedProposal: Proposal }
-  ) {    
-    this.currentUserId$ = this.store.select(selectCurrentUserId); 
-  }
+    constructor(
+        public dialogRef: MatDialogRef<ProposalDialogComponent>,
+        private dataService: DataService,
+        private store: Store,
+        @Inject(MAT_DIALOG_DATA) 
+        public data: { selectedProposal: Proposal }
+    ) {    
+        this.currentUserId$ = this.store.select(selectCurrentUserId); 
+        this.users$ = this.store.select(selectUsers); 
+    }
 
-  async ngOnInit() {
-    this.usersFromFile = await this.dataService.getUsers();
-  }
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
 
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
-  submitCounterProposal(): void {
-    const item = this.data.selectedProposal; 
+    submitCounterProposal(): void {
+        const item = this.data.selectedProposal; 
     
-    this.currentUserId$.pipe(take(1)).subscribe((userId) => {
+        this.currentUserId$.pipe(take(1)).subscribe((userId) => {
         if (!item || !item.ownerIds || userId === null) return;
-
-    //  don't include the current user
-        const usersResponses: { userId: number; accept: boolean }[] = this.usersFromFile
+    
+        this.users$.pipe(take(1)).subscribe(users => {
+            const usersResponses: { userId: number; accept: boolean }[] = users
             .filter(user => 
-            item.ownerIds.includes(user.partyId) && user.id !== userId)
+                item.ownerIds.includes(user.partyId) && user.id !== userId)
             .map(user => ({ userId: user.id, accept: false }));
-
-        const counterProposalData: Proposal = {
+    
+            const counterProposalData: Proposal = {
             id: Math.random().toString(36).substr(2, 9),
             itemId: item.itemId,
             userId: userId,
             ownerIds: item.ownerIds,
             paymentRatios: item.ownerIds.reduce((acc, ownerId) => {
-            acc[ownerId] = this.paymentRatios[ownerId] || 0;
-            return acc;
+                acc[ownerId] = this.paymentRatios[ownerId] || 0;
+                return acc;
             }, {} as { [key: number]: number }), 
             comment: this.comment || '',
             createdAt: new Date(),
             status: 'Pending',
             counterProposalToId: item.id,
             usersResponses,
-        };
-
-        this.store.dispatch(counterProposal({ 
+            };
+    
+            this.store.dispatch(counterProposal({ 
             proposalId: item.id, 
             newProposal: counterProposalData 
-        }));
-
-        this.store.dispatch(rejectProposal({ proposalId: item.id }));
-        
-        this.dialogRef.close(counterProposalData);
+            }));
+    
+            this.store.dispatch(rejectProposal({ proposalId: item.id }));
+            
+            this.dialogRef.close(counterProposalData);
+        });
     });
   }
-}
+}  
--- src/app/components/proposal-form/proposal-form.component.ts
+++ src/app/components/proposal-form/proposal-form.component.ts
@@ -1,10 +1,9 @@
 import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 import { Proposal } from '../../models/proposal.model';
-import { DataService } from '../../services/data.services';
 import { Item } from '../../models/items.model';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { selectCurrentUserId } from '../../../store/user/user.selectors';
+import { selectCurrentUserId, selectUsers } from '../../../store/user/user.selectors';
 import { take } from 'rxjs/operators'; 
 import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
@@ -20,59 +19,60 @@ import { selectProposalState } from '../../../store/proposal/proposal.selector';
   styleUrls: ['./proposal-form.component.css'],
   imports: [ProposalFormComponent, MatFormField, MatLabel, FormsModule, MatInput, CommonModule, MatFormFieldModule, MatInputModule],
 })
-export class ProposalFormComponent implements OnInit {
+export class ProposalFormComponent {
   @Input() item: Item | null = null;
   @Output() submitProposal = new EventEmitter<Proposal[]>();
   currentUserId$: Observable<number | null>;
-  usersFromFile: User[] = [];
+  users$: Observable<User[]>; 
 
   comment: string = '';
   paymentRatios: { [ownerId: number]: number } = {};
   proposalCreationSuccess: boolean | null = null; 
   proposalCreationError: string | null = null; 
 
-  constructor(private dataService: DataService, private store: Store) {
+  constructor(private store: Store) {
     this.currentUserId$ = this.store.select(selectCurrentUserId);
+    this.users$ = this.store.select(selectUsers); 
   }
 
-  async ngOnInit() {
-    this.usersFromFile = await this.dataService.getUsers();
-  }
 
-  createProposal() {
+  createProposal(): void {
     const item = this.item; 
     this.currentUserId$.pipe(take(1)).subscribe((userId) => {
       if (!item || !item.ownerIds || userId === null) return;
-      const usersResponses: { userId: number; accept: boolean }[] = this.usersFromFile
-        .filter(user => 
-          item.ownerIds.includes(user.partyId) && user.id !== userId)
-        .map(user => ({ userId: user.id, accept: false }));
 
-      const proposal: Proposal = {
-        id: Math.random().toString(36).substr(2, 9),
-        itemId: item.id,
-        userId: userId,
-        ownerIds: item.ownerIds,
-        paymentRatios: item.ownerIds.reduce((acc, ownerId) => {
-          acc[ownerId] = this.paymentRatios[ownerId] || 0;
-          return acc;
-        }, {} as { [key: number]: number }), 
-        comment: this.comment || '',
-        createdAt: new Date(),
-        status: 'Pending',
-        usersResponses: usersResponses,
-      };
-
-      this.store.dispatch(createProposal({ proposal }));
-      
-      this.store.select(selectProposalState).pipe(take(1)).subscribe(state => {
-        if (state.creationSuccess) {
-          alert("Proposal created successfully!");
-        }
-        if (state.creationError) {
-          alert(`Error: ${state.creationError}`);
-        }
+      this.users$.pipe(take(1)).subscribe(users => {
+        const usersResponses: { userId: number; accept: boolean }[] = users
+          .filter(user => 
+            item.ownerIds.includes(user.partyId) && user.id !== userId)
+          .map(user => ({ userId: user.id, accept: false }));
+  
+        const proposal: Proposal = {
+          id: Math.random().toString(36).substr(2, 9),
+          itemId: item.id,
+          userId: userId,
+          ownerIds: item.ownerIds,
+          paymentRatios: item.ownerIds.reduce((acc, ownerId) => {
+            acc[ownerId] = this.paymentRatios[ownerId] || 0;
+            return acc;
+          }, {} as { [key: number]: number }), 
+          comment: this.comment || '',
+          createdAt: new Date(),
+          status: 'Pending',
+          usersResponses: usersResponses,
+        };
+  
+        this.store.dispatch(createProposal({ proposal }));
+        
+        this.store.select(selectProposalState).pipe(take(1)).subscribe(state => {
+          if (state.creationSuccess) {
+            alert("Proposal created successfully!");
+          }
+          if (state.creationError) {
+            alert(`Error: ${state.creationError}`);
+          }
+        });
       });
     });
   }
-}
+}  
