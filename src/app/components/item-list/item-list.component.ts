85baf81f03452c34acc99af5498057c3bfbb711d
Gabriela411 <gabriela.toncu@yahoo.com>
2025-02-18 14:59:13 +0000

    Add Icon to highlight items on which some action is expected

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { map, Observable, of, switchMap } from 'rxjs';
 import { Item } from '../../models/items.model';
 import { selectItem } from '../../../store/item/item.actions';
 import { selectItemsByUser, selectSelectedItem } from '../../../store/item/item.selectors';
@@ -9,6 +9,8 @@ import { MatListModule } from '@angular/material/list';
 import { ItemsState } from '../../../store/item/item.reducer';
 import { ProposalFormComponent } from '../proposal-form/proposal-form.component';
 import { ProposalHistoryComponent } from '../proposal-history/proposal-history.component';
+import { selectProposalsForItem } from '../../../store/proposal/proposal.selector';
+import { selectCurrentUserId } from '../../../store/user/user.selectors';
 
 @Component({
   selector: 'item-list',
@@ -20,12 +22,30 @@ import { ProposalHistoryComponent } from '../proposal-history/proposal-history.c
 export class ItemListComponent {
   userItems$: Observable<Item[]>;
   selectedItem$: Observable<Item | null>;
+  currentUserId$: Observable<number | null>;
 
   constructor(private store: Store<ItemsState>) {
+    this.currentUserId$ = this.store.select(selectCurrentUserId);
     this.userItems$ = this.store.select(selectItemsByUser);
     this.selectedItem$ = this.store.select(selectSelectedItem);
   }
 
+  hasPendingProposals(itemId: number): Observable<boolean> {
+    return this.store.select(selectProposalsForItem(itemId)).pipe(
+      switchMap((proposals) => {
+        if (!proposals) return of(false);
+        
+        return this.currentUserId$.pipe(
+          map((currentUserId) => {
+            if (currentUserId === null) return false; 
+
+            const userProposals = proposals[currentUserId] || [];
+            return userProposals.some(proposal => proposal.status === 'Pending');
+          })
+        );
+      })
+    );
+  }
   // ngOnInit(): void {
   //   this.store.dispatch(loadItemsByUser());
 
