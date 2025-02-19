4076ecf3fd867218a12f212122ea62dddb3278b1
Gabriela411 <gabriela.toncu@yahoo.com>
2025-02-19 19:56:38 +0000

    Add spacing

--- src/store/item/item.actions.ts
+++ src/store/item/item.actions.ts
@@ -2,12 +2,36 @@ import { createAction, props } from '@ngrx/store';
 import { Item } from '../../app/models/items.model';
 
 export const loadItems = createAction('[Item] Load Items');
-export const loadItemsSuccess = createAction('[Item] Load Items Success', props<{ items: Item[] }>());
-export const loadItemsFailure = createAction('[Item] Load Items Failure', props<{ error: any }>());
+
+export const loadItemsSuccess = createAction(
+    '[Item] Load Items Success', 
+    props<{ items: Item[] }>()
+);
+
+export const loadItemsFailure = createAction(
+    '[Item] Load Items Failure', 
+    props<{ error: any }>()
+);
 
 export const loadItemsByUser = createAction('[Item] Load Items By User');
-export const loadItemsByUserSuccess = createAction('[Item] Load Items By User Success', props<{ items: Item[] }>());
-export const loadItemsByUserFailure = createAction('[Item] Load Items By User Failure', props<{ error: any }>());
+
+export const loadItemsByUserSuccess = createAction(
+    '[Item] Load Items By User Success', 
+    props<{ items: Item[] }>()
+);
+
+export const loadItemsByUserFailure = createAction(
+    '[Item] Load Items By User Failure', 
+    props<{ error: any }>()
+);
+
 // export const filterItemsByUser = createAction('[Item] Filter Items By User', props<{ userId: number }>());
-export const selectItem = createAction('[Item] Select Item', props<{ item: Item | null }>()); 
-export const updateItem = createAction('[Item] Update Item', props<{ item: Item }>());
+export const selectItem = createAction(
+    '[Item] Select Item', 
+    props<{ item: Item | null }>()
+); 
+
+export const updateItem = createAction(
+    '[Item] Update Item', 
+    props<{ item: Item }>()
+);
--- src/store/owner/owner.actions.ts
+++ src/store/owner/owner.actions.ts
@@ -2,5 +2,13 @@ import { createAction, props } from '@ngrx/store';
 import { Owner } from '../../app/models/owner.model';
 
 export const loadOwners = createAction('[Owner] Load Owners');
-export const loadOwnersSuccess = createAction('[Owner] Load Owners Success', props<{ owners: Owner[] }>());
-export const loadOwnersFailure = createAction('[Owner] Load Owners Failure', props<{ error: any }>());
+
+export const loadOwnersSuccess = createAction(
+    '[Owner] Load Owners Success', 
+    props<{ owners: Owner[] }>()
+);
+
+export const loadOwnersFailure = createAction(
+    '[Owner] Load Owners Failure', 
+    props<{ error: any }>()
+);
