1b4758d96f587bd67dd1c1b9243c978596377a43
Gabriela411 <gabriela.toncu@yahoo.com>
2025-02-21 17:36:33 +0000

    Update items-history-container.component.ts

--- src/app/components/items-history-container/items-history-container.component.ts
+++ src/app/components/items-history-container/items-history-container.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { HistoryContainerComponent } from '../proposal-history/history-container.component';
 import { ItemsContainerComponent } from '../items-container/items-container.component';
+import { NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'items-history-container',
   templateUrl: './items-history-container.component.html',
   styleUrls: ['./items-history-container.component.scss'],
-  imports: [HistoryContainerComponent, ItemsContainerComponent],
+  imports: [HistoryContainerComponent, ItemsContainerComponent, NgIf, MatIconModule],
 })
 export class ItemsHistoryConatinerComponent {
 
-    constructor() {}
+  constructor(private eRef: ElementRef) {}
+  
+    isItemsVisible: boolean = false;
+    isSmallScreen: boolean = window.innerWidth <= 1024;
+  
+    toggleItemsContainer() {
+      this.isItemsVisible = !this.isItemsVisible;
+    }
+  
+    onItemsContainerClick() {
+      this.isItemsVisible = false; 
+    }
+
+    @HostListener('document:click', ['$event'])
+    handleClickOutside(event: Event) {
+      if (this.isSmallScreen && this.isItemsVisible && !this.eRef.nativeElement.contains(event.target)) {
+        this.isItemsVisible = false;
+      }
+    }
+  
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+      this.isSmallScreen = window.innerWidth <= 768;
+    }
 }
